83cca91438367df6d10683663863924c41c48765
Tyree Christensen <tyree83@gmail.com>
2021-01-14 12:36:53 -0500

    created cat. controller, add views category

--- db/migrate/20210114173202_create_products.rb
+++ db/migrate/20210114173202_create_products.rb
@@ -1,9 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
-      t.string :title, null:false
-      t.text :description
-      t.decimal :price, null:false, precision: 10, scale: 2
 
       t.timestamps
     end

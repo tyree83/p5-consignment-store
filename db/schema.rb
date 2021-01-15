5b5410d3e269726c435039c9a5ebc996a7126428
Tyree Christensen <tyree83@gmail.com>
2021-01-15 11:56:18 -0500

    CreatedProductCategories-belongs to cat&prod

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_163612) do
+ActiveRecord::Schema.define(version: 2021_01_15_163942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_01_15_163612) do
     t.index ["title"], name: "index_categories_on_title", unique: true
   end
 
+  create_table "product_categories", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title", limit: 150, null: false
     t.decimal "price", precision: 15, scale: 2, default: "0.0", null: false
@@ -31,4 +40,6 @@ ActiveRecord::Schema.define(version: 2021_01_15_163612) do
     t.index ["title"], name: "index_products_on_title", unique: true
   end
 
+  add_foreign_key "product_categories", "categories", name: "fk_product_categories_to_categories"
+  add_foreign_key "product_categories", "products", name: "fk_product_categories_to_products"
 end

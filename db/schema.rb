b19ebc866c4a9858fe269b88f6812980ace8cbe5
Tyree Christensen <tyree83@gmail.com>
2021-01-15 11:38:50 -0500

    added category model, changed title length

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_155843) do
+ActiveRecord::Schema.define(version: 2021_01_15_163612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title", limit: 100, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_categories_on_title", unique: true
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title", limit: 150, null: false

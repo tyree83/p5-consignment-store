e37bb08d6fd9b2e64e1876d5d4da65228f229c2a
Tyree Christensen <tyree83@gmail.com>
2021-01-16 08:28:11 -0500

    got shopping cart working

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,5 +1,9 @@
 class OrderItemsController < ApplicationController
 
+  def index
+    @items = current_cart.order.items
+  end
+
   def create
     current_cart.add_item(
       product_id: params[:product_id],
@@ -8,3 +12,10 @@ class OrderItemsController < ApplicationController
 
     redirect_to cart_path
   end
+
+  def destroy
+    current_cart.remove_item(id: params[:id])
+    redirect_to cart_path
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_224232) do
+ActiveRecord::Schema.define(version: 2021_01_16_131842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,10 +35,11 @@ ActiveRecord::Schema.define(version: 2021_01_15_224232) do
 
   create_table "orders", force: :cascade do |t|
     t.string "first_name"
-    t.string "last_name", null: false
+    t.string "last_name"
     t.decimal "sub_total", precision: 15, scale: 2, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
   end
 
   create_table "product_categories", force: :cascade do |t|

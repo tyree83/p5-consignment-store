125e757be1e1a5599e6745bd7f44cec842d7925f
Tyree Christensen <tyree83@gmail.com>
2021-01-28 13:25:02 -0500

    ready for deploying

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
+    render json: { status: 200, message: "consignment_store" }
     @categories = Category.joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
   end
 

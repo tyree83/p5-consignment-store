8a6e264492510f89339c97b443fd41f353de61eb
Tyree Christensen <tyree83@gmail.com>
2021-02-05 16:06:25 -0500

    deploy to heroku

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
 
   def index
     render json: { status: 200, message: "consignment_store" }
-    @categories = Category.joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
+    resourrces :@categories = Category.joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
   end
 
 end

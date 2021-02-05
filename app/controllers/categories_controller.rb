89554c8f8b9dedc48ca1f35551b0298210505ef5
Tyree Christensen <tyree83@gmail.com>
2021-02-05 16:47:57 -0500

    make it better

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
 
   def index
     render json: { status: 200, message: "consignment_store" }
-    resources :@categories = Category.joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
+    @categories = Category.joins(:products).select('categories.*, count(products.id) as products_count').group('categories.id').order(:title)
   end
 
 end
